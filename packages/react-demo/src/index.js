b749e8377aad0aa5da92c375fe5fc1a5dbf9c33f
limingyang <865713897@qq.com>
2022-03-01 16:38:36 +0800

    2022-03-01基础搭建

---
+++ packages/react-demo/src/index.js
@@ -0,0 +1,10 @@
+import MiniReact from "./MiniReact";
+
+const vistualDom = (
+  <div className="container">
+    <div>sdsd</div>
+  </div>
+);
+
+console.log(vistualDom);
+// console.log(MiniReact);
